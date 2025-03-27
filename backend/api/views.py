4ce9bd4698799755bf6e1483c906323ca158493b
WojciechWnek <wownek@gmail.com>
2025-03-27 15:36:08 +0100

    Utilize serializer validation

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,8 +5,11 @@ from rest_framework.decorators import api_view
 from products.models import Product
 from products.serializers import ProductSerializer
 
-@api_view(['GET'])
+@api_view(['post'])
 def api_home(request, *args, **kwargs):
-    instance = Product.objects.all().order_by('?').first()
-    data = ProductSerializer(instance).data
-    return Response(data)
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        instance = serializer.save()
+        print(instance)
+
+        return Response(serializer.data)
