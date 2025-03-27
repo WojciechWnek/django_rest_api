da3dc23ad37e2386643b2378a6fa604249c37254
WojciechWnek <wownek@gmail.com>
2025-03-27 13:48:50 +0100

    model_to_dict

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,14 @@
-from django.http import JsonResponse
+import json
 
+from django.http import JsonResponse, HttpResponse
+from django.forms.models import model_to_dict
 from products.models import Product
-
 
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by('?').first()
     data = {}
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
-    return JsonResponse(data, safe=False)
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+    return JsonResponse(data)
+    #     json_data_str = json.dumps(data)
+    # return HttpResponse(json_data_str, safe=False, headers={"Content-Type": "application/json"})
--- py_client/basic.py
+++ py_client/basic.py
@@ -5,5 +5,6 @@ import requests
 endpoint = " http://localhost:8000/api/"
 
 get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello World!"})
-print(get_response.text)
-print(get_response.status_code)
+# print(get_response.headers)
+# print(get_response.text)
+print(get_response.json())
