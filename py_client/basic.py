4a63bd954178c68432bbe69d852fa9bd4be0d9dc
WojciechWnek <wownek@gmail.com>
2025-03-27 15:20:36 +0100

    Add serializer to product

--- py_client/basic.py
+++ py_client/basic.py
@@ -6,5 +6,5 @@ endpoint = " http://localhost:8000/api/"
 
 get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello World!"})
 # print(get_response.headers)
-# print(get_response.text)
-print(get_response.json())
+print(get_response.text)
+# print(get_response.json())
