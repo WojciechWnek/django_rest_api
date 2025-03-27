08f34ad09caffb263c951ae4e97c40ee50a2965c
WojciechWnek <wownek@gmail.com>
2025-03-27 11:47:54 +0100

    Manually echo back data from request

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,16 @@
 from django.http import JsonResponse
+import json
 
 def api_home(request):
-    return JsonResponse({"message": "This is JSON response"})
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+
+
+    data['headers'] = dict(request.headers)
+    data['params'] = dict(request.GET)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,6 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = " http://localhost:8000/api/"
 
-get_response = requests.get(endpoint)
+get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello World!"})
 print(get_response.text)
-print(get_response.json()["message"])
 print(get_response.status_code)
