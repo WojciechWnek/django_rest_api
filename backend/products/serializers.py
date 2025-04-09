0da407ace46fac71659bd5dcffb6a707040aafa3
WojciechWnek <wownek@gmail.com>
2025-04-09 11:01:08 +0200

    Pagination

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -14,66 +14,26 @@ class ProductInlineSerializer(serializers.Serializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     owner = UserPublicSerializer(source="user",read_only=True)
-    related_products = ProductInlineSerializer(source='user.products.all' ,many=True, read_only=True)
-    my_discount = serializers.SerializerMethodField(read_only=True)
-    url = serializers.SerializerMethodField(read_only=True)
-    edit_url = serializers.SerializerMethodField(read_only=True)
-
-    preferred_url = serializers.HyperlinkedIdentityField(
+    url = serializers.HyperlinkedIdentityField(
         view_name='product-detail',
         lookup_field='pk',
     )
-
-    # email = serializers.EmailField(source="user.email",read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
     title = serializers.CharField(validators=[validate_title_no_hello, unique_product_title])
-    # name = serializers.CharField(source='title', read_only=True)
-
-    my_user_data = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Product
         fields = [
             'owner',
-            'preferred_url',
-            "edit_url",
             'url',
-            # 'email',
+            "edit_url",
             'pk',
             'title',
-            # "name",
             'content',
             'price',
             'sale_price',
-            'my_discount',
-            'my_user_data',
-
-            'related_products'
         ]
 
-    def get_my_user_data(self, obj):
-        return {
-            "username": obj.user.username,
-        }
-
-    # def validate_title(self,value):
-    #     request = self.context.get('request')
-    #     user = request.user
-    #     qs = Product.objects.filter(user=user,title__iexact=value)
-    #     if qs.exists():
-    #         raise serializers.ValidationError(f"{value} is already a product name")
-    #     return value
-
-
-    # def create(self, validated_data):
-    #     # return Product.objects.create(**validated_data
-    #     email = validated_data.pop('email')
-    #     obj =  super().create(validated_data)
-    #     # print(email, obj)
-    #     return obj
-    #
-    # def update(self, instance, validated_data):
-    #     email = validated_data.pop('email')
-    #     return super().update(instance, validated_data)
 
     def get_edit_url(self, obj):
         request = self.context.get('request')
@@ -87,9 +47,3 @@ class ProductSerializer(serializers.ModelSerializer):
             return None
         return reverse("product-detail", kwargs={"pk": obj.pk},request = request)
 
-    def get_my_discount(self, obj):
-        if not hasattr(obj, 'id'):
-            return None
-        if not isinstance(obj, Product):
-            return None
-        return obj.get_discount()
