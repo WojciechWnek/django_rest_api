fb53d8a3ac99adb92eac3465ffc3fea290845d2c
WojciechWnek <wownek@gmail.com>
2025-04-09 13:42:32 +0200

    A Django Based Search for our Product API

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,13 +1,38 @@
 from django.conf import settings
 from django.db import models
+from django.db.models import Q
 
 User = settings.AUTH_USER_MODEL
+
+class ProductQuerySet(models.QuerySet):
+    def is_public(self):
+        return self.filter(public=True)
+
+    def search(self, query, user=None):
+        lookup = Q(title__icontains=query) | Q(content__icontains=query)
+        qs = self.is_public().filter(lookup)
+        if user is not None:
+            qs2 = self.filter(user=user).filter(lookup)
+            qs = (qs2 | qs).distinct()
+        return qs
+
+class ProductManager(models.Manager):
+
+    def get_queryset(self, *args, **kwargs):
+        return ProductQuerySet(self.model, using=self._db)
+
+    def search(self,query, user=None):
+        return self.get_queryset().search(query, user=user)
+
 
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, default=1, related_name='products')
     title = models.CharField(max_length=120)
     content = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=15, default=99.99)
+    public = models.BooleanField(default=True)
+
+    objects = ProductManager()
 
     @property
     def sale_price(self):
